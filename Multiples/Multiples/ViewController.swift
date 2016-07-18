f16ebbb92db40b7a557607608c3954329f5419a3
Maurice Ben-Oduro <moduro@iocreate.attlocal.net>
2016-07-18 10:00:40 -0400

    Added displayNilValueAlert func

--- Multiples/Multiples/ViewController.swift
+++ Multiples/Multiples/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     // Properties
     @IBOutlet weak var multiplesInput: UITextField!
     @IBOutlet weak var resultsDisplay: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,12 +35,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func playButtonPressed(sender: AnyObject) {
-        
+        if multiplesInput.text != nil && multiplesInput.text != "" {
+            playButton.hidden = true
+            submitButton.hidden = false
+        } else {
+            displayNilValueAlert()
+        }
     }
     
     @IBAction func submitButtonPressed(sender: AnyObject) {
     }
     
+    func displayNilValueAlert (){
+        
+        // Setup a constant to UIAlertController Class
+        let nilMsgAlertViewController = UIAlertController(title: "Alert", message: "Please enter a value", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        // Provide and action for the constant
+        nilMsgAlertViewController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+        
+        // Display alert
+        self.presentViewController(nilMsgAlertViewController, animated: true, completion: nil)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
